d8ea163bea9db8dcd05b62ef09eeff2e3797e3e6
KunYi Chen <kunyi.chen@gmail.com>
2019-09-08 15:33:44 +0800

    added crc16 library
    currently the library support the below algorithm
    
    * CRC16-ARC
    * CRC16-MODBUS
    * CRC16-KERMIT
    
    build & verify with MSVC 2019

---
+++ crc16/crc16.c
@@ -0,0 +1,135 @@
+﻿
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include "CRC16.h"
+
+static uint16_t _table[256];
+
+// ref. http://reveng.sourceforge.net/crc-catalogue/16.htm
+const PARAM_CRC_T CRC16_PARAM_ARC = {
+    .poly = 0x8005,
+    .init = 0x0000,
+    .xorOut = 0x0000,
+    .check = 0xbb3d,
+    .residue = 0x0000,
+    .refIn = true,
+    .refOut = true
+};
+
+const PARAM_CRC_T CRC16_PARAM_MODBUS = {
+    .poly = 0x8005,
+    .init = 0xFFFF,
+    .xorOut = 0x0000,
+    .check = 0x4b37,
+    .residue = 0x0000,
+    .refIn = true,
+    .refOut = true
+};
+
+const PARAM_CRC_T CRC16_PARAM_KERMIT = {
+    .poly = 0x1021,
+    .init = 0x0000,
+    .xorOut = 0x0000,
+    .check = 0x2189,
+    .residue = 0x0000,
+    .refIn = true,
+    .refOut = true
+};
+
+static PARAM_CRC_T _alg = {
+    .poly = 0x8005,
+    .init = 0x0000,
+    .xorOut = 0x0000,
+    .check = 0xbb3d,
+    .residue = 0x0000,
+    .refIn = true,
+    .refOut = true
+};
+const static uint8_t rtab16[] = {
+                    0,  8, 4, 12,
+                    2, 10, 6, 14,
+                    1,  9, 5, 13,
+                    3, 11, 7, 15 };
+
+static uint8_t reverseU8(const uint8_t data) {
+    return (rtab16[data & 0xF] << 4) |
+           (rtab16[data >> 4 & 0xF]);
+}
+
+static uint16_t reverseU16(const uint16_t data) {
+
+    uint16_t num = 0;
+    for (uint8_t i = 0 ; i < 4; i++)
+        num |= ((uint16_t)rtab16[data >> (i * 4) & 0xF]) << (12 - (i * 4));
+    return num;
+}
+
+void makeCRCTable(const PARAM_CRC_T *params)
+{
+    uint16_t crc;
+
+    if (params != NULL) {
+        memcpy(&_alg, params, sizeof(params));
+    }
+
+    for (uint16_t n = 0; n < 256; n++) {
+        crc = n << 8;
+        for (uint16_t i = 0; i < 8; i++) {
+            crc = (crc & 0x8000) ?
+                (crc << 1) ^ _alg.poly :
+                crc << 1;
+        }
+        _table[n] = crc;
+    }
+}
+
+uint16_t calcCRC16(const uint8_t* pData, const size_t len)
+{
+    uint8_t tmp;
+    uint16_t ret = _alg.init;
+
+    for (unsigned i = 0; i < len; i++)
+    {
+        tmp = (_alg.refIn) ? reverseU8(pData[i]) : pData[i];
+        ret = ret << 8 ^ _table[(ret >> 8) ^ tmp];
+    }
+    return (_alg.refOut) ?
+        reverseU16(ret) ^ _alg.xorOut :
+        ret ^ _alg.xorOut;
+}
+
+
+#ifdef _MSC_VER
+#if _MSC_VER > 1910 /* from VS2017, C99 */
+int main()
+{
+    const uint8_t test[] = "123456789";
+    #define numAlg  (3) // number of algorithm
+    const PARAM_CRC_T* testParam[numAlg]= {
+        &CRC16_PARAM_ARC,
+        &CRC16_PARAM_MODBUS,
+        &CRC16_PARAM_KERMIT
+    };
+    const uint8_t *strAlg[numAlg] = {
+        "CRC16/ARC",
+        "CRC16/MODBUS",
+        "CRC16/KERMIT"
+    };
+
+    uint16_t result;
+    uint16_t check;
+
+    for (unsigned i = 0; i < numAlg; i++) {
+        makeCRCTable(testParam[i]);
+        result = calcCRC16(test, strlen(test));
+        check = testParam[i]->check;
+        printf("%s:0x%04x, expected:0x%04x\r\n", strAlg[i], result, check);
+        printf("  calc result:%s\r\n", (result != check) ? "Failed" : "Passed");
+    }
+
+    return 0;
+}
+#endif
+#endif
---
+++ crc16/crc16.h
@@ -0,0 +1,29 @@
+﻿
+#pragma once
+
+#ifndef _CRC16_H_
+#define _CRC16_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct _crc16_param_t {
+    uint16_t poly;  // ploy function
+    uint16_t init;  // initial val
+    uint16_t xorOut;
+    uint16_t check; // for "123456789" result
+    uint16_t residue;
+    bool     refIn;
+    bool     refOut;
+} PARAM_CRC_T;
+
+// predefinition algorithm
+extern const PARAM_CRC_T 	CRC16_PARAM_ARC;
+extern const PARAM_CRC_T 	CRC16_PARAM_MODBUS;
+extern const PARAM_CRC_T 	CRC16_PARAM_KERMIT;
+
+// export function
+extern void makeCRCTable(const PARAM_CRC_T *params);
+extern uint16_t calcCRC16(const uint8_t* pData, const size_t len);
+
+#endif /* end of _CRC16_H_ */
